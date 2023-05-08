78e71587c459d5b2c1df4698bbda774bbbb12fae
Mila <hexlet@DESKTOP-BFF4B7D.localdomain>
2023-05-08 10:55:44 +0400

    2 step done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,18 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1');
+
+program
+    .helpOption('-h, --help', 'output usage informaion')
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log(filepath1, filepath2);
+    })
+    
+    
+    
 
 program.parse();
